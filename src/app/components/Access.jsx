8f53acf06c47a30d6c6709c0de763ac348ef1d01
Anezz12 <harsenaargrtya1@gmail.com>
2024-06-15 20:30:18 +0700

    update

--- src/app/components/Access.jsx
+++ src/app/components/Access.jsx
@@ -1,12 +1,19 @@
+import Image from "next/image";
 export default function Access() {
   return (
     <section>
-      <div>
+      <div className="max-w-6xl mx-auto text-center px-10 my-20">
         <h3>Access Clipboard Anywhere</h3>
         <p>
           Whether you’re on the go, or at your computer, you can access all your
           Clipboard snippets in a few simple clicks.
         </p>
+        <Image
+          width={500}
+          height={500}
+          alt="pict"
+          src="/Image/image-devices.png"
+        />
       </div>
     </section>
   );
