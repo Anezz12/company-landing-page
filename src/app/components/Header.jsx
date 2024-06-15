b7f2e61990fddce5f59b981e1a84c195238e162f
Anezz12 <harsenaargrtya1@gmail.com>
2024-06-15 13:58:20 +0700

    first

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto text-center mb-40 px-10 pt-16">
+        <Image
+          src="./Image/logo.svg"
+          width={200}
+          height={300}
+          alt=""
+          className="mx-auto my-16"
+        />
+        <h3 className=" mb-8 text-4xl font-bold text-darkGrayishBlue">
+          A History of everything you copy
+        </h3>
+        <p className="max-w-3xl mx-auto mb-10 text-2xl text-grayishBlue">
+          Clipboard allows you to track and organize everything you copy.
+          Instantly access your clipboard on all your devices.
+        </p>
+        {/* Button Container */}
+        <div className="flex flex-col justify-center w-full space-y-6 text-xl text-white md:flex-row md:space-y-0 md:space-x-4">
+          <div className="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80 cursor-pointer">
+            Download for Ios
+          </div>
+          <div className="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80 cursor-pointer">
+            Download for Mac
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
